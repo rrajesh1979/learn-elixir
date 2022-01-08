de4c0574d47a0daab79a508917b094a45d3e696b
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-08 11:05:08 -0500

    :fire: Using Flow

--- paymentproc/lib/paymentproc.ex
+++ paymentproc/lib/paymentproc.ex
@@ -1,5 +1,6 @@
 defmodule Paymentproc do
   alias NimbleCSV.RFC4180, as: CSV
+
   @moduledoc """
   Documentation for `Paymentproc`.
   """
@@ -9,22 +10,24 @@ defmodule Paymentproc do
   end
 
   def open_payments() do
- 	  payments_csv()
- 	  |> File.read!()
- 	  |> CSV.parse_string()
- 	  |> Enum.map(fn row ->
- 	    %{
- 	      id: Enum.at(row, 0),
- 	      tenant: Enum.at(row, 1),
- 	      type: Enum.at(row, 2),
-        value: Enum.at(row, 3),
- 	      created_at: Enum.at(row, 4),
-        status: Enum.at(row,5)
- 	    }
- 	  end)
- 	  |> Enum.reject(&(&1.status == "CLOSED"))
- 	end
+    payments_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
 
+      %{
+        id: Enum.at(row, 0),
+        tenant: Enum.at(row, 1),
+        type: Enum.at(row, 2),
+        value: Enum.at(row, 3),
+        created_at: Enum.at(row, 4),
+        status: Enum.at(row, 5)
+      }
+    end)
+    |> Flow.reject(&(&1.status == "CLOSED" || &1.status == "status"))
+    |> Enum.to_list()
+  end
 
   # def read_file(filepath) do
 
@@ -38,5 +41,4 @@ defmodule Paymentproc do
   #   |> Enum.to_list()
 
   # end
-
 end
