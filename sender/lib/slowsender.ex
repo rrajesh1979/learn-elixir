b53ed73aeabf570c2f48429ef54bff7f608fad5c
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-12 18:58:50 -0500

    :art: Learning Elixir concurrent tasks. Starting with simple Task module usage.

---
+++ sender/lib/slowsender.ex
@@ -0,0 +1,15 @@
+defmodule SlowSender do
+  def send_email(email) do
+    Process.sleep(30_000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    Enum.each(emails, fn email ->
+      Task.start(fn ->
+        send_email(email)
+      end)
+    end)
+  end
+end
